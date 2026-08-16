e95d1eea9c3476b9354dc29b3cfa98b29bdbe7c5
KolYanCHiKK <texno1106@gmail.com>
2026-08-17 00:01:45 +0300

    Update Logger formater

--- pkg/logs/fornater.go
+++ pkg/logs/fornater.go
@@ -16,11 +16,9 @@ type PrettyFormatter struct {
 
 func InitLogger() {
 	log.SetOutput(os.Stdout)
-	//log.SetFormatter(&PrettyFormatter{
-	//	ShowTimestamp: true,
-	//})
-
-	log.SetFormatter(&log.JSONFormatter{TimestampFormat: "2006-01-02T15:04:05.000Z07:00"})
+	log.SetFormatter(&PrettyFormatter{
+		ShowTimestamp: true,
+	})
 
 	log.SetLevel(log.DebugLevel)
 }
